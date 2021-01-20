9d7c789c5a46c6b3a71d1817e2924df23e478b20
Mads Akselsen <akselsenmads@gmail.com>
2021-01-20 22:13:22 +0100

    new function to swap between API data and localstorage data

--- src/index.ts
+++ src/index.ts
@@ -23,13 +23,13 @@ export function addToFavorites(itemUrl: string) {
 export function loadFromLocalStorage() {
   if (localStorage.getItem('nasaFavorites')) {
     favorites = JSON.parse(localStorage.getItem('nasaFavorites'));
-    console.log('storage favorites', favorites);
+    return favorites;
   }
 }
 
 const loadImages = async () => {
   nasaDataArray = await fetchAPIPictures();
-  updateDOM(nasaDataArray);
+  updateDOM('favorites', nasaDataArray);
 };
 
 // On Load
